e76b68b4bf00111ac08fc438ef68dc0756a5dc78
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-24 12:15:45 -0300

    feat

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configurando a URL base do backend
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://estudo-vercel.vercel.app/',
 });
 
 export default api;
