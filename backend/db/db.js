48b2b94a525c879f86922be57777d2d87ce4ca1f
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-24 11:29:58 -0300

    fix: add pg package

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,9 +3,18 @@ import { Sequelize } from "sequelize";
 // console.log("db",process.env.DATABASE_URL ? "chegou" : "nao chegou");
 // const db = process.env.DATABASE_URL ? "chegou" : "Nao chegou"
 
+// Verifica se o pg está instalado
+try {
+  require('pg');
+} catch (error) {
+  console.error('ERRO: Pacote pg não instalado. Rode: npm install pg');
+  process.exit(1);
+}
+
 const db = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
-	host: "localhost",
+	logging: false,
+	// host: "localhost",
 });
 
 db.authenticate()
