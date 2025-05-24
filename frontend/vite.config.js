be8745ba9c5bde6ff79f15f8526704a3e8e749d3
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-24 12:30:31 -0300

    feat

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    open: true
   }
 })
