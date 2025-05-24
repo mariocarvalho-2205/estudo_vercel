1bfe7b4bf8f75f99cd10ca76ec09819cb672dc9b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-24 15:50:09 -0300

    atualiza configurações CORS

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configurando a URL base do backend
 const api = axios.create({
-  baseURL: 'https://estudo-vercel.vercel.app',
+  baseURL: 'https://estudo-vercel-r5nx.vercel.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
